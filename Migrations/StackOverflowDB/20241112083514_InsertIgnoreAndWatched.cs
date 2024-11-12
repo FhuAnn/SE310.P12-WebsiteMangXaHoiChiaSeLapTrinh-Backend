7b78535c50ca322822e4380db4aa87506471ef79
Ho Pham Phu An <22520013@gm.uit.edu.vn>
2024-11-12 17:22:34 +0700

    commit

---
+++ Migrations/StackOverflowDB/20241112083514_InsertIgnoreAndWatched.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SE310.P12_WebsiteMangXaHoiChiaSeLapTrinh.Migrations.StackOverflowDB
+{
+    /// <inheritdoc />
+    public partial class InsertIgnoreAndWatched : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    role_name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    description = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    created_at = table.Column<DateTime>(type: "datetime", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__roles__3213E83F79CC5BDF", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tags",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    tagname = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__tags__3213E83F865609AA", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    username = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    gravatar = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    views = table.Column<int>(type: "int", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__users__3213E83F1998A5B6", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IgnoreTags",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TagId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IgnoredAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IgnoreTags", x => new { x.UserId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_IgnoreTags_tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "tags",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IgnoreTags_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "posts",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    title = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    body = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    views = table.Column<int>(type: "int", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime", nullable: false),
+                    user_id = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    upvote = table.Column<int>(type: "int", nullable: false),
+                    downvote = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__posts__3213E83F57D81F9F", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__posts__user_id__3F466844",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_roles",
+                columns: table => new
+                {
+                    user_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    role_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    assigned_at = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__user_rol__6EDEA15368399088", x => new { x.user_id, x.role_id });
+                    table.ForeignKey(
+                        name: "FK__user_role__role___5535A963",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK__user_role__user___5441852A",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WatchedTags",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TagId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WatchedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WatchedTags", x => new { x.UserId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_WatchedTags_tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "tags",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WatchedTags_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "answers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    body = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime", nullable: false),
+                    user_id = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    post_id = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    upvote = table.Column<int>(type: "int", nullable: false),
+                    downvote = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__answers__3213E83F376D8E7A", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__answers__post_id__4AB81AF0",
+                        column: x => x.post_id,
+                        principalTable: "posts",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK__answers__user_id__49C3F6B7",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "comments",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    body = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime", nullable: false),
+                    user_id = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    post_id = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__comments__3213E83F2401F5AB", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__comments__post_i__46E78A0C",
+                        column: x => x.post_id,
+                        principalTable: "posts",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK__comments__user_i__45F365D3",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "posttag",
+                columns: table => new
+                {
+                    post_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    tag_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__posttag__4AFEED4DC067451F", x => new { x.post_id, x.tag_id });
+                    table.ForeignKey(
+                        name: "FK__posttag__post_id__4222D4EF",
+                        column: x => x.post_id,
+                        principalTable: "posts",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK__posttag__tag_id__4316F928",
+                        column: x => x.tag_id,
+                        principalTable: "tags",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_answers_post_id",
+                table: "answers",
+                column: "post_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_answers_user_id",
+                table: "answers",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comments_post_id",
+                table: "comments",
+                column: "post_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comments_user_id",
+                table: "comments",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IgnoreTags_TagId",
+                table: "IgnoreTags",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_posts_user_id",
+                table: "posts",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_posttag_tag_id",
+                table: "posttag",
+                column: "tag_id");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__roles__783254B10E1D0E3F",
+                table: "roles",
+                column: "role_name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__tags__D48789A0064EDE44",
+                table: "tags",
+                column: "tagname",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_roles_role_id",
+                table: "user_roles",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__users__F3DBC5726DF6499A",
+                table: "users",
+                column: "username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WatchedTags_TagId",
+                table: "WatchedTags",
+                column: "TagId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "answers");
+
+            migrationBuilder.DropTable(
+                name: "comments");
+
+            migrationBuilder.DropTable(
+                name: "IgnoreTags");
+
+            migrationBuilder.DropTable(
+                name: "posttag");
+
+            migrationBuilder.DropTable(
+                name: "user_roles");
+
+            migrationBuilder.DropTable(
+                name: "WatchedTags");
+
+            migrationBuilder.DropTable(
+                name: "posts");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+
+            migrationBuilder.DropTable(
+                name: "tags");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ Models/Domain/WatchedTag.cs
@@ -0,0 +1,13 @@
+﻿namespace SE310.P12_WebsiteMangXaHoiChiaSeLapTrinh.Models.Domain
+{
+    public class WatchedTag
+    {
+        public Guid UserId { get; set; }
+        public virtual User? User { get; set; }
+
+        public Guid TagId { get; set; }
+        public virtual Tag? Tag { get; set; }
+
+        public DateTime WatchedAt { get; set; } = DateTime.Now;
+    }
+}
