2984c9682e90d771c24a609b8186b6462e9f1578
AndreNguyen03 <123562712+AndreNguyen03@users.noreply.github.com>
2024-11-12 14:01:08 +0700

    commit to pull

--- Models/StackOverflowDBContext.cs
+++ Models/StackOverflowDBContext.cs
@@ -32,10 +32,7 @@ public partial class StackOverflowDBContext : DbContext
 
     public virtual DbSet<UserRole> UserRoles { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-7UOGDK8A;Initial Catalog=stack_overflow8;User = sa, Password = phuan03042004; Integrated Security=True;Trust Server Certificate=True");
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Answer>(entity =>
