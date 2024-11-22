67cc4c81466428445209d8c01f650b9e7a158f0b
Ho Pham Phu An <22520013@gm.uit.edu.vn>
2024-11-22 13:34:18 +0700

    fix 2nd requirement

---
+++ Models/DTO/Get/HomePosttagDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SE310.P12_WebsiteMangXaHoiChiaSeLapTrinh.Models.DTO.Get
+{
+    public class HomePostTagDto
+    {
+        public Guid TagId { get; set; }
+        public virtual PostDto Post { get; set; } = null!;
+        public virtual TagDto Tag { get; set; } = null!;
+    }
+}
