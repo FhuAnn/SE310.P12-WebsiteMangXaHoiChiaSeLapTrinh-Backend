1ac81c973521e8b80507178add1810d964ccce57
AndreNguyen03 <123562712+AndreNguyen03@users.noreply.github.com>
2024-12-07 21:50:00 +0700

    config context

--- Models/Domain/Stackoverflow1511Context.cs
+++ Models/Domain/Stackoverflow1511Context.cs
@@ -37,9 +37,6 @@ public partial class Stackoverflow1511Context : DbContext
     public virtual DbSet<IgnoredTag> IgnoredTags { get; set; }
 
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-7R66M1N;Initial Catalog=stackoverflow1511; Integrated Security=True;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
